c24d981de0435d2fe232537374ab247a0e4751bd
Aditya <4adi26@gmail.com>
2024-06-15 14:59:37 +0530

    create onboard and setup uploadthing

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -19,6 +19,8 @@ import { ChangeEvent, useState } from "react";
 import { Textarea } from "../ui/textarea";
 import { isBase64Image } from "@/lib/utils";
 import { useUploadThing } from "@/lib/uploadthing";
+import { updateUser } from "@/lib/actions/user.actions";
+import { usePathname, useRouter } from "next/navigation";
 
 interface Props {
   user: {
@@ -35,8 +37,11 @@ interface Props {
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
 
+  const router = useRouter();
+  const pathname = usePathname();
   const [files, setFiles] = useState<File[]>([])
   const { startUpload } = useUploadThing("media")
+
 
   const form = useForm<z.infer<typeof UserValidation>>({
     resolver: zodResolver(UserValidation),
@@ -52,12 +57,12 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
     e.preventDefault();
     const fileReader = new FileReader();
 
-    if(e.target.files && e.target.files.length>0){
+    if (e.target.files && e.target.files.length > 0) {
       const file = e.target.files[0]
 
       setFiles(Array.from(e.target.files))
 
-      if(!file.type.includes('image')) return;
+      if (!file.type.includes('image')) return;
 
       fileReader.onload = async (event) => {
         const imageDataUrl = event.target?.result?.toString() || ''
@@ -68,19 +73,35 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
     }
   }
 
-  const onSubmit = async(values: z.infer<typeof UserValidation>) => {
+  const onSubmit = async (values: z.infer<typeof UserValidation>) => {
     const blob = values.profile_photo;
     const hasImageChanged = isBase64Image(blob);
 
-    if(hasImageChanged){
+    if (hasImageChanged) {
       const imgRes = await startUpload(files)
 
-      if(imgRes && imgRes[0].fileUrl){
+      if (imgRes && imgRes[0].fileUrl) {
         values.profile_photo = imgRes[0].fileUrl;
       }
     }
 
     // TODO: Update user profile
+    await updateUser(
+      {
+        userId: user.id,
+        username: values.username,
+        name: values.name,
+        bio: values.bio,
+        image: values.profile_photo,
+        path: pathname
+      }
+    )
+
+    if(pathname === '/profile/edit'){
+      router.back()
+    }else{
+      router.push('/')
+    }
   }
 
   return (
