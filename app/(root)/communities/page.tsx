2c33e2593e8c6547164118ea9d446d1912485ddf
Aditya <4adi26@gmail.com>
2024-06-22 19:49:40 +0530

    add search bar and pagination

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,47 +1,65 @@
 import CommunityCard from "@/components/cards/CommunityCard";
+import Pagination from "@/components/shared/Pagination";
+import Searchbar from "@/components/shared/Searchbar";
 import { fetchCommunities } from "@/lib/actions/community.action";
 import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-const Page = async () => {
+const Page = async ({
+  searchParams
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) => {
   const user = await currentUser();
   if (!user) return null;
 
   const userInfo = await fetchUser(user.id)
   if (!userInfo?.onboarded) redirect('/onboarding')
 
-  // fetch users 
+  // fetch communities 
   const result = await fetchCommunities({
-    searchString: '',
-    pageNumber: 1,
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
     pageSize: 25
   })
 
   return (
-    <section>
-      <h1 className="head-text mb-10">Search</h1>
+    <>
+      <section>
+        <h1 className="head-text mb-10">Search</h1>
 
-      <div className="mt-14 flex flex-col gap-9">
-        {result.communities.length === 0 ? (
-          <p className="no-result">No communities</p>
-        ) : (
-          <>
-            {result.communities.map((community) => (
-              <CommunityCard
-                key={community.id}
-                id={community.id}
-                name={community.name}
-                username={community.username}
-                imgUrl={community.image}
-                bio={community.bio}
-                members={community.members}
-              />
-            ))}
-          </>
-        )}
-      </div>
-    </section>
+        <div className='mt-5'>
+          <Searchbar routeType='communities' />
+        </div>
+
+        <div className="mt-14 flex flex-col gap-9">
+          {result.communities.length === 0 ? (
+            <p className="no-result">No communities</p>
+          ) : (
+            <>
+              {result.communities.map((community) => (
+                <CommunityCard
+                  key={community.id}
+                  id={community.id}
+                  name={community.name}
+                  username={community.username}
+                  imgUrl={community.image}
+                  bio={community.bio}
+                  members={community.members}
+                />
+              ))}
+            </>
+          )}
+        </div>
+      </section>
+
+      <Pagination
+        path='communities'
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
+    </>
   )
 }
 
--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,9 +1,15 @@
 import UserCard from "@/components/cards/UserCard";
+import Pagination from "@/components/shared/Pagination";
+import Searchbar from "@/components/shared/Searchbar";
 import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-const Page = async () => {
+const Page = async ({
+  searchParams
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) => {
   const user = await currentUser();
   if (!user) return null;
 
@@ -13,14 +19,16 @@ const Page = async () => {
   // fetch users 
   const result = await fetchUsers({
     userId: user.id,
-    searchString: '',
-    pageNumber: 1,
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
     pageSize: 25
   })
 
   return (
     <section>
       <h1 className="head-text mb-10">Search</h1>
+
+      <Searchbar routeType="search" />
 
       <div className="mt-14 flex flex-col gap-9">
         {result.users.length === 0 ? (
@@ -40,6 +48,13 @@ const Page = async () => {
           </>
         )}
       </div>
+
+      <Pagination
+        path='search'
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
+      
     </section>
   )
 }
